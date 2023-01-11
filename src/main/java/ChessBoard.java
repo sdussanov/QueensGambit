acfd6736a0c3df055603a2f9f54d5dcd2fdfa7a7
SDussanov <dussanov.s@gmail.com>
2023-01-11 23:15:50 +0600

    1.0.0 first commit

---
+++ src/main/java/ChessBoard.java
@@ -0,0 +1,18 @@
+public class ChessBoard {
+    int[][] board;
+    String nowPlayer;
+
+    public ChessBoard(String nowPlayer) {
+        this.nowPlayer = nowPlayer;
+    }
+
+    public String nowPlayerColor(){
+        return nowPlayer;
+    }
+
+    public void moveToPosition(){
+
+    }
+
+    public void printBoard(){}
+}
