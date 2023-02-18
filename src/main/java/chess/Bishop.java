e035c671bd7be7a7e9c8602786063a8bf33a8039
SDussanov <dussanov.s@gmail.com>
2023-02-18 22:51:49 +0600

    1.0.4 changed structure of packages

--- src/main/java/chess/Bishop.java
+++ src/main/java/chess/Bishop.java
@@ -1,5 +1,23 @@
 package chess;
 
-public class Bishop {
+public class Bishop extends ChessPiece {
 
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    protected String getColor() {
+        return null;
+    }
+
+    @Override
+    protected char getSymbol() {
+        return 0;
+    }
+
+    @Override
+    protected boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        return false;
+    }
 }
