043543eaa7b8860b5afe7f8a6d81ac448ab10066
SDussanov <dussanov.s@gmail.com>
2023-02-19 12:40:20 +0600

    1.0.4 changed structure of packages

--- src/main/java/chess/Queen.java
+++ src/main/java/chess/Queen.java
@@ -1,4 +1,23 @@
 package chess;
 
-public class Queen {
+public class Queen extends ChessPiece {
+
+    public Queen(String color) {
+        super(color);
+    }
+
+    @Override
+    protected String getColor() {
+        return null;
+    }
+
+    @Override
+    protected char getSymbol() {
+        return 0;
+    }
+
+    @Override
+    protected boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        return false;
+    }
 }
