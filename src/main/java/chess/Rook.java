72983adef42742ea7a47ad5a63a5e18465bb2ba2
SDussanov <dussanov.s@gmail.com>
2023-02-20 10:28:15 +0600

    1.0.5 added some logic to the rook

--- src/main/java/chess/Rook.java
+++ src/main/java/chess/Rook.java
@@ -1,5 +1,23 @@
 package chess;
 
-public class Rook {
+public class Rook extends ChessPiece {
 
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    protected String getColor() {
+        return null;
+    }
+
+    @Override
+    protected char getSymbol() {
+        return 0;
+    }
+
+    @Override
+    protected boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        return false;
+    }
 }
