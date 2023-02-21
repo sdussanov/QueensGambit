ab9e5459a776c042246c8597962d3ad9ca2c167d
SDussanov <dussanov.s@gmail.com>
2023-02-21 12:56:29 +0600

    1.0.6 added some logic to the king

--- src/main/java/chess/King.java
+++ src/main/java/chess/King.java
@@ -1,4 +1,23 @@
 package chess;
 
-public class King {
+public class King extends ChessPiece {
+
+    public King(String color) {
+        super(color);
+    }
+
+    @Override
+    protected String getColor() {
+        return null;
+    }
+
+    @Override
+    protected char getSymbol() {
+        return 0;
+    }
+
+    @Override
+    protected boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        return false;
+    }
 }
