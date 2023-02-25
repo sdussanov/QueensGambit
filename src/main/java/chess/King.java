ed8b3f56213266f841444a55b887cf53cd0b8cf5
SDussanov <safarkhan.dussanov@gmail.com>
2023-02-25 20:27:20 +0000

    1.0.10 added main logic to the king

--- src/main/java/chess/King.java
+++ src/main/java/chess/King.java
@@ -7,17 +7,46 @@ public class King extends ChessPiece {
     }
 
     @Override
-    protected String getColor() {
-        return null;
+    public String getColor() {
+        return color;
     }
 
     @Override
-    protected char getSymbol() {
-        return 0;
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+            if (Math.abs(line - toLine) > 1 || Math.abs(column - toColumn) > 1) return false;
+
+            if (isUnderAttack(chessBoard, toLine, toColumn)) return false;
+
+            if (chessBoard.board[toLine][toColumn] != null) {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(color);
+            }
+
+            return true;
+        } else return false;
     }
 
     @Override
-    protected boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+    public char getSymbol() {
+        return 'K';
+    }
+
+    public boolean isUnderAttack(ChessBoard chessBoard, int line, int column) {
+        if (checkPos(line) && checkPos(column)) {
+            for (int i = 0; i < 7; i++) {
+                for (int j = 0; j < 7; j++) {
+                    if (chessBoard.board[i][j] != null) {
+                        if (!chessBoard.board[i][j].getColor().equals(color) && chessBoard.board[i][j].canMoveToPosition(chessBoard, i, j, line, column)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        } else return false;
+    }
+
+    public boolean checkPos(int pos) {
+        return pos >= 0 && pos <= 7;
     }
 }
